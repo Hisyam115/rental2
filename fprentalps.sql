d7682960e21f8d12cd5c046ff73c482ec1deaf87
sammm <156216443+Hisyam115@users.noreply.github.com>
2024-12-27 16:36:59 +0700

    Update fprentalps.sql

--- fprentalps.sql
+++ fprentalps.sql
@@ -11,31 +11,20 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
 -- Database: `fprentalps`
---
 
 -- --------------------------------------------------------
 
---
 -- Table structure for table `device`
---
 
 CREATE TABLE `device` (
-  `id_device` int(10) NOT NULL,
+  `id_device` int(10) NOT NULL AUTO_INCREMENT,
   `device` enum('PS3','PS4','PS5') NOT NULL,
-  `harga` int(10) NOT NULL
+  `harga` int(10) NOT NULL,
+  PRIMARY KEY (`id_device`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
 -- Dumping data for table `device`
---
 
 INSERT INTO `device` (`id_device`, `device`, `harga`) VALUES
 (1, 'PS3', 75000),
@@ -44,22 +33,23 @@ INSERT INTO `device` (`id_device`, `device`, `harga`) VALUES
 
 -- --------------------------------------------------------
 
---
 -- Table structure for table `sewa`
---
 
 CREATE TABLE `sewa` (
-  `id_sewa` int(15) NOT NULL,
+  `id_sewa` int(15) NOT NULL AUTO_INCREMENT,
   `tanggal_sewa` date NOT NULL,
   `tanggal_kembali` date NOT NULL,
   `total_harga` int(10) NOT NULL,
   `device_id` int(10) NOT NULL,
-  `user_id` int(10) NOT NULL
+  `user_id` int(10) NOT NULL,
+  PRIMARY KEY (`id_sewa`),
+  KEY `device_id` (`device_id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `sewa_ibfk_1` FOREIGN KEY (`device_id`) REFERENCES `device` (`id_device`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `sewa_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
 -- Dumping data for table `sewa`
---
 
 INSERT INTO `sewa` (`id_sewa`, `tanggal_sewa`, `tanggal_kembali`, `total_harga`, `device_id`, `user_id`) VALUES
 (3, '2024-06-27', '2024-06-28', 50000, 3, 2),
@@ -74,6 +64,35 @@ INSERT INTO `sewa` (`id_sewa`, `tanggal_sewa`, `tanggal_kembali`, `total_harga`,
 (29, '2024-12-13', '2024-12-16', 300000, 2, 13),
 (30, '2024-12-13', '2024-12-15', 150000, 1, 13),
 (31, '2024-12-19', '2024-12-27', 800000, 2, 6);
+
+-- --------------------------------------------------------
+
+-- Table structure for table `users`
+
+CREATE TABLE `users` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `no_hp` varchar(12) NOT NULL,
+  `alamat` varchar(100) NOT NULL,
+  `role` enum('admin','member') NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Dumping data for table `users`
+
+INSERT INTO `users` (`id`, `username`, `password`, `nama`, `no_hp`, `alamat`, `role`) VALUES
+(1, 'aking', 'password123', 'Krisna', '08123456789', 'Concat', 'admin'),
+(2, 'Bedul', 'memberpass', 'Abdullah Azzam Azed', '08234567890', 'Wonosobo', 'member'),
+(6, 'Asep', '456', 'M. Hisyam Ramadhan', '081234567890', 'Gejayan', 'member'),
+(7, 'krist00n', '123', 'Faesal Krisna Wijaya', '087654321', 'Purworejo', 'member'),
+(11, 'Gupolo', '123', 'Gilang Wahyu Pratama', '00986645634', 'Pingit', 'member'),
+(13, 'lilhadar', 'hadar123', 'Hadar', '08123456789', 'Nganjuk', 'member'),
+(14, 'galih', '12345', 'king repel', '089089893843', 'kotagede', 'member'),
+(15, 'R', 'kntl', 'Rafli', '085555555', 'Bangkok', 'member');
+
 
 -- --------------------------------------------------------
 
