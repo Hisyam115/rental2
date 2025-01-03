22617835587fe1dd535b70efe3a325abcc84a8bc
sammm <156216443+Hisyam115@users.noreply.github.com>
2025-01-03 15:36:41 +0700

    Update fprentalps.sql

--- fprentalps.sql
+++ fprentalps.sql
@@ -3,78 +3,148 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 27, 2024 at 10:46 AM
+-- Generation Time: Jan 03, 2025 at 09:35 AM
 -- Server version: 10.4.32-MariaDB
--- PHP Version: 8.1.25 
+-- PHP Version: 8.1.25
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
--- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
 -- Database: `fprentalps`
+--
+
 -- --------------------------------------------------------
 
+--
 -- Table structure for table `device`
+--
+
 CREATE TABLE `device` (
-  `id_device` int(10) NOT NULL AUTO_INCREMENT,
+  `id_device` int(10) NOT NULL,
   `device` enum('PS3','PS4','PS5') NOT NULL,
-  `harga` int(10) NOT NULL,
-  PRIMARY KEY (`id_device`)
+  `harga` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
 -- Dumping data for table `device`
+--
+
 INSERT INTO `device` (`id_device`, `device`, `harga`) VALUES
 (1, 'PS3', 75000),
 (2, 'PS4', 100000),
 (3, 'PS5', 150000);
 
 -- --------------------------------------------------------
+
+--
 -- Table structure for table `sewa`
+--
+
 CREATE TABLE `sewa` (
-  `id_sewa` int(15) NOT NULL AUTO_INCREMENT,
+  `id_sewa` int(15) NOT NULL,
   `tanggal_sewa` date NOT NULL,
   `tanggal_kembali` date NOT NULL,
   `total_harga` int(10) NOT NULL,
   `device_id` int(10) NOT NULL,
-  `user_id` int(10) NOT NULL,
-  PRIMARY KEY (`id_sewa`),
-  KEY `device_id` (`device_id`),
-  KEY `user_id` (`user_id`),
-  CONSTRAINT `sewa_ibfk_1` FOREIGN KEY (`device_id`) REFERENCES `device` (`id_device`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `sewa_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+  `user_id` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `sewa`
+--
+
+INSERT INTO `sewa` (`id_sewa`, `tanggal_sewa`, `tanggal_kembali`, `total_harga`, `device_id`, `user_id`) VALUES
+(32, '2025-01-03', '2025-01-04', 150000, 3, 2);
+
 -- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
+--
+
 CREATE TABLE `users` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `id` int(10) NOT NULL,
   `username` varchar(50) NOT NULL,
   `password` varchar(50) NOT NULL,
   `nama` varchar(50) NOT NULL,
   `no_hp` varchar(12) NOT NULL,
   `alamat` varchar(100) NOT NULL,
-  `role` enum('admin','member') NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `username` (`username`)
+  `role` enum('admin','member') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
 -- Dumping data for table `users`
+--
+
 INSERT INTO `users` (`id`, `username`, `password`, `nama`, `no_hp`, `alamat`, `role`) VALUES
 (1, 'aking', 'password123', 'Krisna', '08123456789', 'Concat', 'admin'),
 (2, 'Bedul', 'memberpass', 'Abdullah Azzam Azed', '08234567890', 'Wonosobo', 'member');
 
--- --------------------------------------------------------
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `device`
+--
+ALTER TABLE `device`
+  ADD PRIMARY KEY (`id_device`);
+
+--
+-- Indexes for table `sewa`
+--
+ALTER TABLE `sewa`
+  ADD PRIMARY KEY (`id_sewa`),
+  ADD KEY `device_id` (`device_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
 -- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `device`
+--
 ALTER TABLE `device`
   MODIFY `id_device` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
+--
+-- AUTO_INCREMENT for table `sewa`
+--
 ALTER TABLE `sewa`
-  MODIFY `id_sewa` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
+  MODIFY `id_sewa` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
 
+--
+-- AUTO_INCREMENT for table `users`
+--
 ALTER TABLE `users`
   MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `sewa`
+--
+ALTER TABLE `sewa`
+  ADD CONSTRAINT `sewa_ibfk_1` FOREIGN KEY (`device_id`) REFERENCES `device` (`id_device`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `sewa_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
